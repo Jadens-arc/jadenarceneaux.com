e2011ca2338056d360562e878311eb7d1fcfe1bc
jadens-arc <contact@jadenarceneaux.com>
2026-03-24 08:13:55 -0700

    added links for meeting

---
+++ app/links/page.tsx
@@ -0,0 +1,59 @@
+export default function Links() {
+  return (
+    <div className="space-y-14">
+      <h1 className="font-heading text-4xl font-bold tracking-tight sm:text-5xl">
+        Jaden's Links
+      </h1>
+      <ul className="space-y-2 list-disc">
+        <li>
+          <a
+            className="underline underline-offset-4 hover:text-foreground transition-colors"
+            href="https://wheelofnames.com/bpf-vhw"
+          >
+            Wheel #0
+          </a>
+        </li>
+        <li>
+          <a
+            className="underline underline-offset-4 hover:text-foreground transition-colors"
+            href="https://wheelofnames.com/yn5-ug3"
+          >
+            Wheel #1
+          </a>
+        </li>
+        <li>
+          <a
+            className="underline underline-offset-4 hover:text-foreground transition-colors"
+            href="https://wheelofnames.com/p2v-mwg"
+          >
+            Wheel #2
+          </a>
+        </li>
+        <li>
+          <a
+            className="underline underline-offset-4 hover:text-foreground transition-colors"
+            href="https://wheelofnames.com/c6s-tb4"
+          >
+            Wheel #4
+          </a>
+        </li>
+        <li>
+          <a
+            className="underline underline-offset-4 hover:text-foreground transition-colors"
+            href="https://wheelofnames.com/86h-y6y"
+          >
+            Wheel #5
+          </a>
+        </li>
+        <li>
+          <a
+            className="underline underline-offset-4 hover:text-foreground transition-colors"
+            href="https://wheelofnames.com/gh7-uv9"
+          >
+            Wheel #6
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
